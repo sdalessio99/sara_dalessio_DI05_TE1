5e7dc310496eb982494c83881746a8f2078737b6
Sara D'Alessio <sdalessio@birt.eus>
2024-04-19 19:38:10 +0200

    clonacion de la DI04_TE1 para tener las graficas hechas

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'DI04_TE1',
+  appName: 'DI05_TE1',
   webDir: 'www',
   server: {
     androidScheme: 'https'
